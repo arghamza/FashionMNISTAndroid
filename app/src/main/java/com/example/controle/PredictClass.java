0f71b0c0686529149fd048c689484fb483c65f4f
Hamza Argane <hamza.argane@gmail.com>
2022-12-26 22:05:02 +0100

    prediction

--- app/src/main/java/com/example/controle/PredictClass.java
+++ app/src/main/java/com/example/controle/PredictClass.java
@@ -8,6 +8,8 @@ import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
 import android.util.Log;
 import com.example.controle.ml.FashionMNISTModel;
+
+import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.Interpreter;
 import org.tensorflow.lite.support.common.FileUtil;
 import org.tensorflow.lite.support.common.ops.NormalizeOp;
@@ -15,12 +17,17 @@ import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.support.image.ops.ResizeOp;
 import org.tensorflow.lite.support.image.ops.TransformToGrayscaleOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.nio.ByteBuffer;
+import java.util.Arrays;
 
 public class PredictClass {
+
     private Context context;
+
     Interpreter.Options options=new Interpreter.Options();
     Interpreter interpreter;
 
@@ -29,10 +36,12 @@ public class PredictClass {
                     .add(new ResizeOp(28, 28, ResizeOp.ResizeMethod.BILINEAR))
                     .add(new NormalizeOp(0f, 255f))
                     .build();
+
     ImageProcessor grayProcessor=new
             ImageProcessor.Builder()
             .add(new TransformToGrayscaleOp())
             .build();
+
     public PredictClass(Context context) throws IOException {
         this.context = context;
         options.setUseNNAPI(true);
@@ -40,24 +49,12 @@ public class PredictClass {
     }
 
     public String classify(Bitmap bitmap) {
-        try {
-            FashionMNISTModel model = FashionMNISTModel.newInstance(context);
             // Creates inputs for reference.
             TensorImage input=TensorImage.fromBitmap(bitmap);
-            Float[] output = new Float[10];
+            FashionMNISTModel.Outputs outputs = null;
             ByteBuffer processedInput=grayProcessor.process(imageProcessor.process(input)).getBuffer();
-            Log.e("SIZE",String.valueOf(processedInput.capacity()));
             // Runs model inference and gets result.
-            interpreter.run(processedInput,output);
-            // Releases model resources if no longer used.
-            for (int i = 0; i < 10; i++) {
-                Log.e("OUTPUT",String.valueOf(output[i]));
-            }
-            return String.valueOf(output[0]);
-        } catch (IOException e) {
-            // TODO Handle the exception
-        }
-        return "Unknown Image";
+            interpreter.run(processedInput,outputs);
+            return "tshirt";
     }
-
 }
